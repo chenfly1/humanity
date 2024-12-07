db88d1c21124e8228f8168a22fef13f10c84d60d
chenfly1 <52518566+chenfly1@users.noreply.github.com>
2024-12-07 22:27:28 +0800

    Update refferreward.py

--- refferreward.py
+++ refferreward.py
@@ -9,7 +9,7 @@ headers = {
     "accept-language": "zh-CN,zh;q=0.9",
     "authorization": "",
     "content-length": "0",
-    "cookie": "QueueITAccepted-SDFrts345E-V3_humanityprotocol=EventId%3Dhumanityprotocol%26QueueId%3D44b20842-13d0-4be8-a1b8-bd14b4baf4f4%26RedirectType%3Dqueue%26IssueTime%3D1731857430%26Hash%3Dbbec3c989fca03aab9bece8b6b144742ad6498e95ccba2fb17c94b81b2f4b511",
+    "cookie": "AjBOmW2JBjgDyPz35D7/YciYN7KErx60xzerWuXAHOvIvRWOghrnXa7NEzmOHtMqH9T3ZEC/Pb6sg72CHVIgMYJ6WLbRq9xPkv4BXWeiRA+QTBtiqzAFhuEcdpY224ewXc47roBiDRh8rljollB5VDL5BfZeRcrLtawwF0PGTbPetvCVMgA3w6do3d2VAiTq3Czd10Pj7yr2WAfk4kUf0lfbupmNkUnevNh6vlmp9Y8swK3x/QyB8nye/DkVTdvC",
     "origin": "https://testnet.humanity.org",
     "priority": "u=1, i",
     "referer": "https://testnet.humanity.org/dashboard",
